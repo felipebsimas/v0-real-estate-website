970c9b38b1b62cbbce5626035c77f2076bc7aa65
Felipe Batista Simas <48572016+felipebsimas@users.noreply.github.com>
2025-09-05 16:26:07 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = 'localhost'; // Seu hostname de acordo com a Hostinger
+const port = process.env.PORT || 3000; // A porta que a Hostinger te fornece
+
+// Ao usar 'next', a aplicação será iniciada para produção
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true);
+      await handle(req, res, parsedUrl);
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err);
+      res.statusCode = 500;
+      res.end('internal server error');
+    }
+  })
+    .once('error', (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+});
